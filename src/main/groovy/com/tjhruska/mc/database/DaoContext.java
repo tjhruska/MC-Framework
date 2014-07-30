4ae802cf556d83cf2e1767ceaf1ae50748f44f77
Timothy Hruska <tjhruska@yahoo.com>
2014-07-29 23:18:26 -0500

    Adding local date data type, and fixed formatting on some files

--- src/main/groovy/com/tjhruska/mc/database/DaoContext.java
+++ src/main/groovy/com/tjhruska/mc/database/DaoContext.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.tjhruska.mc.database;
 
@@ -10,154 +10,208 @@ import org.joda.time.DateTime;
 
 import com.tjhruska.mc.database.restrictions.Restriction;
 
-
 /**
- * Class DaoContext utilizes a loosely coupled list backed storage mechanism
- * to provide CRUD operations on T that extends BaseDomain (domain classes).  
- * It does not currently propagate saves.  (This could be implemented with 
- * reflection, or loading maps of local containers to doa for that container, 
- * and implementing some type of dirty checking.)  It does not handle composite pks.
+ * Class DaoContext utilizes a loosely coupled list backed storage mechanism to
+ * provide CRUD operations on T that extends BaseDomain (domain classes). It
+ * does not currently propagate saves. (This could be implemented with
+ * reflection, or loading maps of local containers to doa for that container,
+ * and implementing some type of dirty checking.) It does not handle composite
+ * pks.
+ * 
  * @author tjhruska
  *
  */
-public class DaoContext <T extends BaseDomain> implements DaoDomain<T> {
+public class DaoContext<T extends BaseDomain> implements DaoDomain<T> {
 
-	private Class<T> type;
-	private List<T> storageContainer;
-	
-	/**
-	 * @param type
-	 * @param storageContainer
-	 */
-	public DaoContext(Class<T> type, List<T> storageContainer) {
-		super();
-		this.type = type;
-		this.storageContainer = storageContainer;
-		System.out.println("type:" + type + " size:" + storageContainer.size());
-		int id = 1;
-		for (T item : storageContainer){
-			item.setId(id);
-			item.setAddDate(new DateTime());
-			id++;
-		}
-	}
+  private Class<T> type;
+  private List<T> storageContainer;
 
-	
-	private Integer markPersisted(T domainObject){
-		//for now we will assume anything with an id is persisted
-		Integer id = domainObject.getId();
-		if (id == null){
-			id = getMaxId();
-			id++;
-			domainObject.setId(id);
-			domainObject.setAddDate(new DateTime());
-		}
-		return id;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#getDomainClass()
-	 */
-	public Class<T> getDomainClass() {
-		return type;
-	}
-
-  /* (non-Javadoc)
-   * @see com.tjhruska.mc.database.DaoDomain#save(com.tjhruska.mc.database.BaseDomain)
+  /**
+   * @param type
+   * @param storageContainer
    */
+  public DaoContext(Class<T> type, List<T> storageContainer) {
+    super();
+    this.type = type;
+    this.storageContainer = storageContainer;
+    System.out.println("type:" + type + " size:" + storageContainer.size());
+    int id = 1;
+    for (T item : storageContainer) {
+      item.setId(id);
+      item.setAddDate(new DateTime());
+      id++;
+    }
+  }
+
+  private Integer markPersisted(T domainObject) {
+    // for now we will assume anything with an id is persisted
+    Integer id = domainObject.getId();
+    if (id == null) {
+      id = getMaxId();
+      id++;
+      domainObject.setId(id);
+      domainObject.setAddDate(new DateTime());
+    }
+    return id;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.tjhruska.mc.database.DaoDomain#getDomainClass()
+   */
+  @Override
+  public Class<T> getDomainClass() {
+    return type;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#save(com.tjhruska.mc.database.BaseDomain
+   * )
+   */
+  @Override
   public Integer save(T domainObject) {
     Integer id = domainObject.getId();
     if (id == null) {
       id = markPersisted(domainObject);
     }
     boolean isInStorageContainer = false;
-    for (T item : storageContainer){
+    for (T item : storageContainer) {
       if (item.getId().equals(domainObject.getId()))
         isInStorageContainer = true;
     }
     if (!isInStorageContainer)
       storageContainer.add(domainObject);
     else
-      throw new RuntimeException("id: " + id + " is already in the store, call saveOrUpdate instead.");
+      throw new RuntimeException("id: " + id
+          + " is already in the store, call saveOrUpdate instead.");
     return id;
   }
-  
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DomainDao#saveOrUpdate(com.tjhruska.mc.database.BaseDomain)
-	 */
-	public Integer saveOrUpdate(T domainObject) {
-		Integer id = markPersisted(domainObject);
-		boolean isInStorageContainer = false;
-		for (T item : storageContainer){
-			if (item.equals(domainObject))
-				isInStorageContainer = true;
-		}
-		if (!isInStorageContainer)
-			storageContainer.add(domainObject);
-		return id;
-	}
 
-	/**
-	 * @return the max id currently in use in storageContainer
-	 */
-	private int getMaxId(){
-		int currentId = 0;
-		for(T current : storageContainer){
-			if (current.getId() > currentId)
-				currentId = current.getId();
-		}
-		return currentId;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DomainDao#delete(com.tjhruska.mc.database.BaseDomain)
-	 */
-	public void delete(T domainObject) {
-		storageContainer.remove(domainObject);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DomainDao#saveOrUpdate(com.tjhruska.mc.database
+   * .BaseDomain)
+   */
+  @Override
+  public Integer saveOrUpdate(T domainObject) {
+    Integer id = markPersisted(domainObject);
+    boolean isInStorageContainer = false;
+    for (T item : storageContainer) {
+      if (item.equals(domainObject))
+        isInStorageContainer = true;
+    }
+    if (!isInStorageContainer)
+      storageContainer.add(domainObject);
+    return id;
+  }
 
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DomainDao#findByPK(java.io.Serializable)
-	 */
-	public T findByPK(Serializable id) {
-		if (id == null) return null;
-		for (T element : storageContainer){
-			if ( element.getId().equals(id))
-				return element;
-		}
-		return null;
-	}
+  /**
+   * @return the max id currently in use in storageContainer
+   */
+  private int getMaxId() {
+    int currentId = 0;
+    for (T current : storageContainer) {
+      if (current.getId() > currentId)
+        currentId = current.getId();
+    }
+    return currentId;
+  }
 
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DomainDao#findAll()
-	 */
-	public List<T> findAll() {
-		return storageContainer;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DomainDao#delete(com.tjhruska.mc.database.BaseDomain
+   * )
+   */
+  @Override
+  public void delete(T domainObject) {
+    storageContainer.remove(domainObject);
+  }
 
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database.restrictions.Restriction)
-	 */
-	public List<T> findByCriteria(Restriction restriction) {
-		throw new UnsupportedOperationException();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.tjhruska.mc.database.DomainDao#findByPK(java.io.Serializable)
+   */
+  @Override
+  public T findByPK(Serializable id) {
+    if (id == null)
+      return null;
+    for (T element : storageContainer) {
+      if (element.getId().equals(id))
+        return element;
+    }
+    return null;
+  }
 
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database.restrictions.Restriction, Integer)
-	 */
-	public List<T> findByCriteria(Restriction restriction, Integer limit) {
-		throw new UnsupportedOperationException();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.tjhruska.mc.database.DomainDao#findAll()
+   */
+  @Override
+  public List<T> findAll() {
+    return storageContainer;
+  }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction)
+   */
+  @Override
+  public List<T> findByCriteria(Restriction restriction) {
+    throw new UnsupportedOperationException();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction, Integer)
+   */
+  @Override
+  public List<T> findByCriteria(Restriction restriction, Integer limit) {
+    throw new UnsupportedOperationException();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see com.tjhruska.mc.database.DaoDomain#clearCache()
    */
+  @Override
   public void evict(T domainObject) {
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see com.tjhruska.mc.database.DaoDomain#flush()
    */
+  @Override
   public void flush() {
   }
-} 
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findOneByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction)
+   */
+  @Override
+  public T findOneByCriteria(Restriction restriction) {
+    throw new UnsupportedOperationException();
+  }
+}
--- src/main/groovy/com/tjhruska/mc/database/DaoHibernate.java
+++ src/main/groovy/com/tjhruska/mc/database/DaoHibernate.java
@@ -1,10 +1,11 @@
 /**
- * 
+ *
  */
 package com.tjhruska.mc.database;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -18,38 +19,40 @@ import com.tjhruska.mc.database.restrictions.Restriction;
 import com.tjhruska.mc.database.restrictions.RestrictionHelper;
 
 /**
- * Class DaoHibernate utilizes a loosely coupled hibernate session factory
- * to provide CRUD operations on T that extends BaseDomain (domain classes).
+ * Class DaoHibernate utilizes a loosely coupled hibernate session factory to
+ * provide CRUD operations on T that extends BaseDomain (domain classes).
+ *
  * @author tjhruska
  *
  */
-public class DaoHibernate <T extends BaseDomain> implements DaoDomain<T> {
-	private static final Logger log
-		= LoggerFactory.getLogger(DaoHibernate.class);
+public class DaoHibernate<T extends BaseDomain> implements DaoDomain<T> {
+  private static final Logger log = LoggerFactory.getLogger(DaoHibernate.class);
 
-	private Class<T> type;
-	private SessionFactory sessionFactory;
-	private Session session;
-	@Autowired
-	private RestrictionHelper restrictionHelper;
-	
-	/**
-	 * @param type
-	 */
-	public DaoHibernate(Class<T> type){
-		super();
-		this.type = type;
-		//System.out.println("DaoHibernate constructer was passed a Class<T>=" + type);
-	}
-	
-	/**
-	 * @param sessionFactory the sessionFactory to set
-	 */
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+  private Class<T> type;
+  private SessionFactory sessionFactory;
+  private Session session;
+  @Autowired
+  private RestrictionHelper restrictionHelper;
 
-	/**
+  /**
+   * @param type
+   */
+  public DaoHibernate(Class<T> type) {
+    super();
+    this.type = type;
+    // System.out.println("DaoHibernate constructer was passed a Class<T>=" +
+    // type);
+  }
+
+  /**
+   * @param sessionFactory
+   *          the sessionFactory to set
+   */
+  public void setSessionFactory(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
+  /**
    * @return the session
    */
   public Session getSession() {
@@ -59,9 +62,10 @@ public class DaoHibernate <T extends BaseDomain> implements DaoDomain<T> {
       return sessionFactory.getCurrentSession();
     }
   }
-  
+
   /**
-   * @param session the session to set
+   * @param session
+   *          the session to set
    */
   public void setSession(Session session) {
     this.session = session;
@@ -80,103 +84,162 @@ public class DaoHibernate <T extends BaseDomain> implements DaoDomain<T> {
   public void closeSession() {
     this.session.close();
   }
-  
+
   /**
-	 * @param restrictionHelper the restrictionHelper to set
-	 */
-	public void setRestrictionhelper(RestrictionHelper restrictionHelper) {
-		this.restrictionHelper = restrictionHelper;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#getDomainClass()
-	 */
-	public Class<T> getDomainClass() {
-		return type;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#save(com.tjhruska.mc.database.BaseDomain)
-	 */
-	public Integer save(T domainObject) {
-	  getSession().save(domainObject);
-	  return domainObject.getId();
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#saveOrUpdate(com.tjhruska.mc.database.BaseDomain)
-	 */
-	@Transactional
-	public Integer saveOrUpdate(T domainObject) {
-		getSession().saveOrUpdate(domainObject);
-		return domainObject.getId();
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#delete(com.tjhruska.mc.database.BaseDomain)
-	 */
-	@Transactional
-	public void delete(T domainObject) {
-		getSession().delete(domainObject);
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#findByPK(java.io.Serializable)
-	 */
-	@Transactional
-	@SuppressWarnings("unchecked")
-	public T findByPK(Serializable id) {
-		return (T)getSession().get(type, id);
-	}
-
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#findAll()
-	 */
-	@SuppressWarnings("unchecked")
-	@Transactional
-	public List<T> findAll() {
-		//return (List<T>)sessionFactory.getCurrentSession().createCriteria(type).list();
-		List<T> ts = (List<T>)getSession().createCriteria(type).list();
-		log.info("found {} objects", ts.size());
-		return ts;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database.restrictions.Restriction)
-	 */
-	@Transactional
-	public List<T> findByCriteria(Restriction restriction) {
-		return findByCriteria(restriction, null);
-	}
-
-  /* (non-Javadoc)
-   * @see com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database.restrictions.Restriction, java.lang.Integer)
+   * @param restrictionHelper
+   *          the restrictionHelper to set
    */
-	@Transactional
-	@SuppressWarnings("unchecked")
-  public List<T> findByCriteria(Restriction restriction, Integer limit) {
-		Criteria criteria = getSession().createCriteria(type);
-		if (limit != null) {
-		  criteria.setMaxResults(limit);
-		}
-		restrictionHelper.buildCriterion(criteria, restriction);
-		List<T> ts = (List<T>)criteria.list();
-		log.info("found {} objects", ts.size());
-		return ts;
+  public void setRestrictionhelper(RestrictionHelper restrictionHelper) {
+    this.restrictionHelper = restrictionHelper;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   *
+   * @see com.tjhruska.mc.database.DaoDomain#getDomainClass()
+   */
+  @Override
+  public Class<T> getDomainClass() {
+    return type;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#save(com.tjhruska.mc.database.BaseDomain
+   * )
+   */
+  @Override
+  public Integer save(T domainObject) {
+    getSession().save(domainObject);
+    return domainObject.getId();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#saveOrUpdate(com.tjhruska.mc.database
+   * .BaseDomain)
+   */
+  @Override
+  @Transactional
+  public Integer saveOrUpdate(T domainObject) {
+    getSession().saveOrUpdate(domainObject);
+    return domainObject.getId();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#delete(com.tjhruska.mc.database.BaseDomain
+   * )
+   */
+  @Override
+  @Transactional
+  public void delete(T domainObject) {
+    getSession().delete(domainObject);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see com.tjhruska.mc.database.DaoDomain#findByPK(java.io.Serializable)
+   */
+  @Override
+  @Transactional
+  @SuppressWarnings("unchecked")
+  public T findByPK(Serializable id) {
+    return (T) getSession().get(type, id);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see com.tjhruska.mc.database.DaoDomain#findAll()
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  @Transactional
+  public List<T> findAll() {
+    // return
+    // (List<T>)sessionFactory.getCurrentSession().createCriteria(type).list();
+    List<T> ts = getSession().createCriteria(type).list();
+    log.info("found {} objects", ts.size());
+    return ts;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findOneByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction)
+   */
+  @Override
+  public T findOneByCriteria(Restriction restriction) {
+    List<T> results = findByCriteria(restriction);
+    if (results.size() != 1) {
+      throw new NoSuchElementException(
+          "Expected exactly one record, but found " + results.size());
+    }
+    return results.iterator().next();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction)
+   */
+  @Override
+  @Transactional
+  public List<T> findByCriteria(Restriction restriction) {
+    return findByCriteria(restriction, null);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * com.tjhruska.mc.database.DaoDomain#findByCriteria(com.tjhruska.mc.database
+   * .restrictions.Restriction, java.lang.Integer)
+   */
+  @Override
+  @Transactional
+  @SuppressWarnings("unchecked")
+  public List<T> findByCriteria(Restriction restriction, Integer limit) {
+    Criteria criteria = getSession().createCriteria(type);
+    if (limit != null) {
+      criteria.setMaxResults(limit);
+    }
+    restrictionHelper.buildCriterion(criteria, restriction);
+    List<T> ts = criteria.list();
+    log.debug("found {} objects", ts.size());
+    return ts;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
    * @see com.tjhruska.mc.database.DaoDomain#clearCache()
    */
+  @Override
   public void evict(T domainObject) {
     getSession().evict(domainObject);
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   *
    * @see com.tjhruska.mc.database.DaoDomain#flush()
    */
-	@Transactional
+  @Override
+  @Transactional
   public void flush() {
     getSession().flush();
   }
-} 
+}
