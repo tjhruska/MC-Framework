a97ea9bafc5a8e1915deafbee3e1465a2ac3275d
Timothy Hruska <tjhruska@yahoo.com>
2015-01-01 19:28:06 -0600

    Change domain object comparison to use the domain update comparison method

--- src/main/groovy/com/tjhruska/mc/database/test/GeneratedDomainAndDaoTest.groovy
+++ src/main/groovy/com/tjhruska/mc/database/test/GeneratedDomainAndDaoTest.groovy
@@ -1,83 +1,81 @@
 package com.tjhruska.mc.database.test
 
-import org.junit.After;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*
 
-import com.tjhruska.mc.database.DaoDomain
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.transaction.annotation.Transactional
+
 import com.tjhruska.mc.database.BaseDomain
+import com.tjhruska.mc.database.DaoDomain
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration()
 abstract class GeneratedDomainAndDaoTest {
-  
+
   abstract DaoDomain getDao()
 
   abstract BaseDomain getTestObject(Integer number, Integer sequence)
-  
+
   abstract BaseDomain updateDomainObject(Integer number, BaseDomain domain)
-  
+
   abstract void assertDomainUpdates(BaseDomain expected, BaseDomain actual)
-  
+
   abstract void deleteChildrenIfNeeded(BaseDomain domain)
-  
+
   BaseDomain persistTestObject(Integer number) {
     BaseDomain domain1 = getTestObject(number, 0)
     //assertNull(domain1.getId()) -- the class knows if this should be null or not, either way it will fail to persist if it is wrong, testing here doesn't work
     assertNull(domain1.getAddDate())
-    
+
     getDao().saveOrUpdate(domain1)
     getDao().flush()
     getDao().evict(domain1)
     return domain1
   }
-  
+
   @Test
   void createTest(){
     BaseDomain domain1 = persistTestObject(1)
     assertNotNull(domain1.getId())
   }
-  
+
   @Test
   void readTest(){
     BaseDomain domain1 = persistTestObject(1)
-    
+
     BaseDomain domain1Found = dao.findByPK(domain1.getPK())
-    
-    assertEquals(domain1, domain1Found)
+
+    assertDomainUpdates(domain1, domain1Found)
   }
-  
+
   @Test
   void updateTest(){
     BaseDomain domain1 = persistTestObject(1)
-    
+
     BaseDomain expected = updateDomainObject(3, domain1)
     dao.saveOrUpdate(domain1)
     dao.flush()
     dao.evict(domain1)
- 
+
     BaseDomain domain1Found = dao.findByPK(domain1.getPK())
-    
+
     assertDomainUpdates(expected, domain1Found)
   }
-  
+
   @Test
   void deleteTest(){
     BaseDomain domain1 = persistTestObject(1)
-    
+
     deleteChildrenIfNeeded(domain1)
     dao.delete(domain1)
     dao.flush()
     dao.evict(domain1)
-    
+
     BaseDomain domain1Deleted = dao.findByPK(domain1.getPK())
     assertNull(domain1Deleted)
   }
-} 
+}
