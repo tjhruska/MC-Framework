7470a31b44662a22f61fc750df9351b91dbc97d5
Timothy Hruska <tjhruska@yahoo.com>
2015-03-18 07:55:02 -0500

    Making equality for logging messages with throwables depend only on detailMessage for that throwable.

--- src/main/groovy/com/tjhruska/mc/util/VerifyAppenderLogback.groovy
+++ src/main/groovy/com/tjhruska/mc/util/VerifyAppenderLogback.groovy
@@ -1,88 +1,88 @@
 package com.tjhruska.mc.util
 
-import groovy.transform.CompileStatic;
-import groovy.util.logging.Slf4j;
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
 import java.util.ArrayList
 import java.util.Collections
 import java.util.List
 
 import org.junit.Assert
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.Appender;
-import ch.qos.logback.core.Context;
-import ch.qos.logback.core.LogbackException;
-import ch.qos.logback.core.filter.Filter;
-import ch.qos.logback.core.spi.FilterReply;
-import ch.qos.logback.core.status.Status;
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.LoggingEvent
+import ch.qos.logback.core.Appender
+import ch.qos.logback.core.Context
+import ch.qos.logback.core.LogbackException
+import ch.qos.logback.core.filter.Filter
+import ch.qos.logback.core.spi.FilterReply
+import ch.qos.logback.core.status.Status
 
 import com.google.common.base.Objects
 
 @Slf4j
 public class VerifyAppenderLogback implements Appender, VerifyAppender {
- 
-  Class classUnderTest 
+
+  Class classUnderTest
   private Level verifyLevel
   private Level originalLevel
   private List<LogMessage> loggingToVerify
   Logger logger
-  
+
   public VerifyAppenderLogback(Class classUnderTest, Level verifyLevel) {
     this.classUnderTest = classUnderTest
     this.verifyLevel = verifyLevel
     loggingToVerify = Collections.synchronizedList(new ArrayList<VerifyAppenderLogback.LogMessage>())
-   
-    Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
     if (!originalLevel.equals(verifyLevel)) {
       originalLevel = root.getLevel()
-	    log.info("VerifyAppenderLogback setting logging level to {}, original was {}", verifyLevel, originalLevel)
-	    root.setLevel(verifyLevel);
+      log.info("VerifyAppenderLogback setting logging level to {}, original was {}", verifyLevel, originalLevel)
+      root.setLevel(verifyLevel)
     }
-    
+
     //setup logback
-    logger = (Logger) LoggerFactory.getLogger(classUnderTest);
-    logger.addAppender(this);
+    logger = (Logger) LoggerFactory.getLogger(classUnderTest)
+    logger.addAppender(this)
   }
-  
+
   public void demandLog(Object level, String message, Boolean substringMatch=false) {
     if(((Level)level).levelInt >= verifyLevel.levelInt)
       loggingToVerify.add(new LogMessage(level, message, substringMatch))
   }
-  
+
   public void demandLog(Object level, String message, Exception e, Boolean substringMatch=false) {
     if(((Level)level).levelInt >= verifyLevel.levelInt)
       loggingToVerify.add(new LogMessage(level, message, e, substringMatch))
   }
-  
+
   public void verify() {
     if (originalLevel != null) {
-	    log.info("VerifyAppenderLogback restoring logging level to {}", originalLevel)
-      Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-      root.setLevel(originalLevel);
+      log.info("VerifyAppenderLogback restoring logging level to {}", originalLevel)
+      Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
+      root.setLevel(originalLevel)
     }
-    
+
     logger.detachAppender(this)
     if (!loggingToVerify.isEmpty()) {
       throw new AssertionError("Logging demand registered, but not observed at time of verify: " + loggingToVerify.remove(0))
     }
   }
-  
+
   //logback methods
   @Override
   public void doAppend(Object event) throws LogbackException {
     LoggingEvent le = (ch.qos.logback.classic.spi.LoggingEvent)event
-    
+
     LogMessage actualMessage = new LogMessage(le)
     if (actualMessage.level.levelInt >= verifyLevel.levelInt) {
       if (loggingToVerify.isEmpty()) {
         throw new AssertionError("Missing demand for logger: " + actualMessage)
       }
       LogMessage expectedMessage = loggingToVerify.remove(0)
-    
+
       Assert.assertEquals("Log message didn't match demanded message.", expectedMessage, actualMessage)
     }
   }
@@ -92,37 +92,37 @@ public class VerifyAppenderLogback implements Appender, VerifyAppender {
     private Object message
     private Object throwable
     private Boolean substringMatch
-    
+
     public LogMessage(LoggingEvent loggingEvent) {
       this(loggingEvent.getLevel(), loggingEvent.getFormattedMessage(), loggingEvent.getThrowableProxy()?.getThrowable(), false)
     }
-    
+
     public LogMessage(Level level, String message, Boolean substringMatch) {
       this(level, message, (Throwable)null, substringMatch)
     }
-    
+
     public LogMessage(Level level, Object message, Throwable throwable, Boolean substringMatch) {
       this.level = level
       this.message = message
       this.throwable = throwable
       this.substringMatch = substringMatch
     }
-    
+
     @Override
     public boolean equals(Object object) {
       if (object == null || !(object instanceof LogMessage))
         return false
       LogMessage other = (LogMessage) object
-      return Objects.equal(level, other.level) && Objects.equal(throwable, other.throwable) && 
-        ((substringMatch== true && other.message.startsWith(message)) || Objects.equal(message, other.message) )
+      return Objects.equal(level, other.level) && Objects.equal(throwable.detailMessage, other.throwable.detailMessage) &&
+      ((substringMatch== true && other.message.startsWith(message)) || Objects.equal(message, other.message) )
     }
-    
+
     @Override
     public String toString() {
       return level.toString() + " " + message + (throwable == null ? "" : " (" + throwable.getClass().getName() + ")")
     }
   }
-  
+
   @Override
   public void start() {
     //no-op
@@ -136,7 +136,7 @@ public class VerifyAppenderLogback implements Appender, VerifyAppender {
   @Override
   public boolean isStarted() {
     //no-op
-    return false;
+    return false
   }
 
   @Override
@@ -147,7 +147,7 @@ public class VerifyAppenderLogback implements Appender, VerifyAppender {
   @Override
   public Context getContext() {
     //no-op
-    return null;
+    return null
   }
 
   @Override
@@ -198,21 +198,21 @@ public class VerifyAppenderLogback implements Appender, VerifyAppender {
   @Override
   public List getCopyOfAttachedFiltersList() {
     //no-op
-    return null;
+    return null
   }
 
   @Override
   public FilterReply getFilterChainDecision(Object event) {
     //no-op
-    return null;
+    return null
   }
-  
+
   @Override
   public String getName() {
     //no-op
-    return null;
+    return null
   }
-  
+
   @Override
   public void setName(String name) {
     //no-op
